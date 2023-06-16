7ffcc464a96e51784fd48c1ba116c282e311b3f7
Vakal <05vlad13@gmail.com>
2023-06-16 23:28:11 +0300

    updated get_price.py

--- tg_bot/handlers/users/get_price.py
+++ tg_bot/handlers/users/get_price.py
@@ -17,21 +17,45 @@ def get_coin_id(coin_symbol):
 
 async def get_price(msg: types.Message):
     crypto_space = msg.text.split()
-    crypto_name = crypto_space[1]
-    multiply = float(crypto_space[0])
-    coin_id = get_coin_id(crypto_name.lower())
-    price = cg.get_price(ids=coin_id, vs_currencies='usd', include_24hr_change="true")
-    usd_24h_change = round(price[coin_id]["usd_24h_change"], 2)
+    if len(crypto_space) >= 2: 
+        crypto_name = crypto_space[1]
+        multiply = float(crypto_space[0])
+        coin_id = get_coin_id(crypto_name.lower())
+        price = cg.get_price(ids=coin_id, vs_currencies='usd', include_24hr_change="true")
+        usd_24h_change = round(price[coin_id]["usd_24h_change"], 2)
 
-    if price:
-        price = price[coin_id.lower()]['usd']
-        total_price = int(price) * multiply
+        if price:
+            price = price[coin_id.lower()]['usd']
+            total_price = int(price) * multiply
+        else:
+            await msg.answer("Crypto was not found")
+            return
+        
+        text = f"💰 <b>{crypto_name.upper()} ({coin_id})</b> 🔥\n" \
+               f"\n" \
+               f"📈 <b>Ціна за 1 {crypto_name.upper()}:</b>~{price}💲\n"\
+               f"\n" \
+               f"📈 <b>Ціна за {multiply} {crypto_name.upper()}:</b>~{total_price}💲\n"\
+               f"\n" \
+               f"📌 <b>24 H:</b> {usd_24h_change} % 📊"
+
+        await msg.answer(text=text, parse_mode="html")
+
     else:
-        await msg.answer("Crypto was not found")
-        return
+        crypto_name = crypto_space[0]
+        coin_id = get_coin_id(crypto_name.lower())
+        price = cg.get_price(ids=coin_id, vs_currencies='usd', include_24hr_change="true")
+        usd_24h_change = round(price[coin_id]["usd_24h_change"], 2)
 
-    await msg.answer(
-        f'💰 <b>{crypto_name} ({coin_id})</b> 🔥\n📈 <b>Ціна</b>~{total_price}💲\n📌 <b>24 H:</b> {usd_24h_change} % 📊')
+        if price:
+                price = price[coin_id.lower()]['usd']
+        else:
+            await msg.answer("Crypto was not found")
+            return
+
+
+        await msg.answer(
+            f'💰 <b>{crypto_name} ({coin_id})</b> 🔥\n📈 <b>Ціна</b>~{price}💲\n📌 <b>24 H:</b> {usd_24h_change} % 📊')
 
 
 def register_get_price(dp: Dispatcher):
